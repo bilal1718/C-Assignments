42ecb8e8943639d163b1f6d5c4376df65971bbb5
Muhammad Bilal <muhbil171845@gmail.com>
2024-02-02 18:34:53 +0500

    Update Lab_Task.cpp

--- Lab_Task.cpp
+++ Lab_Task.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 struct Employee{
@@ -13,4 +12,14 @@ void Employee::newEmployee(string name,int employee_id,string department,float s
     cout << employee_id << endl;
     cout << department << endl;
     cout << salary << endl;
+}
+int main(){
+    Employee e1;
+    e1.name="Bilal";
+    e1.employee_id=1;
+    e1.department="Software Engineering";
+    e1.salary=10000;
+    e1.newEmployee(e1.name,e1.employee_id,e1.department,e1.salary);
+
+    return 0;
 }
